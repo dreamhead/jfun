0d82453d2d97705136a6348c34c31ce2774bea24
dreamhead <dreamhead.cn@gmail.com>
2013-03-10 08:16:53 +0800

    added long to string function

--- src/main/java/com/github/dreamhead/jfun/NumberFunctions.java
+++ src/main/java/com/github/dreamhead/jfun/NumberFunctions.java
@@ -3,15 +3,20 @@ package com.github.dreamhead.jfun;
 import com.google.common.base.Function;
 
 public class NumberFunctions {
-    private static final Function<Integer, String> INT_TO_STRING_FUNCTION = new IntToStringFunction();
+    private static final Function<Integer, String> INT_TO_STRING_FUNCTION = new ToStringFunction<Integer>();
+    private static final Function<Long, String> LONG_TO_STRING_FUNCTION = new ToStringFunction<Long>();
 
     public static Function<Integer, String> intToString() {
         return INT_TO_STRING_FUNCTION;
     }
 
-    private static class IntToStringFunction extends NoArityFunction<Integer, String> {
+    public static Function<Long, String> longToString() {
+        return LONG_TO_STRING_FUNCTION;
+    }
+
+    private static class ToStringFunction<T> extends NoArityFunction<T, String> {
         @Override
-        public String apply(Integer input) {
+        public String apply(T input) {
             return input.toString();
         }
     }
--- src/test/java/com/github/dreamhead/jfun/NumberFunctionsTest.java
+++ src/test/java/com/github/dreamhead/jfun/NumberFunctionsTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.util.List;
 
 import static com.github.dreamhead.jfun.NumberFunctions.intToString;
+import static com.github.dreamhead.jfun.NumberFunctions.longToString;
 import static com.google.common.collect.ImmutableList.of;
 import static com.google.common.collect.Iterables.transform;
 import static org.hamcrest.CoreMatchers.is;
@@ -17,6 +18,16 @@ public class NumberFunctionsTest {
         List<Integer> ints = of(1, 2, 3);
 
         Iterable<String> strings = transform(ints, intToString());
+        assertThat(Iterables.get(strings, 0), is("1"));
+        assertThat(Iterables.get(strings, 1), is("2"));
+        assertThat(Iterables.get(strings, 2), is("3"));
+    }
+
+    @Test
+    public void should_convert_long_to_string() {
+        List<Long> longs = of(1l, 2l, 3l);
+
+        Iterable<String> strings = transform(longs, longToString());
         assertThat(Iterables.get(strings, 0), is("1"));
         assertThat(Iterables.get(strings, 1), is("2"));
         assertThat(Iterables.get(strings, 2), is("3"));
