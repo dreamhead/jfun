46dfbae36d1c6e68569367d6b972b383b442addb
dreamhead <dreamhead.cn@gmail.com>
2013-03-13 06:29:24 +0800

    added double to string function

--- src/main/java/com/github/dreamhead/jfun/NumberFunctions.java
+++ src/main/java/com/github/dreamhead/jfun/NumberFunctions.java
@@ -5,6 +5,7 @@ import com.google.common.base.Function;
 public class NumberFunctions {
     private static final Function<Integer, String> INT_TO_STRING_FUNCTION = new ToStringFunction<Integer>();
     private static final Function<Long, String> LONG_TO_STRING_FUNCTION = new ToStringFunction<Long>();
+    private static final Function<Double, String> DOUBLE_TO_STRING_FUNCTION = new ToStringFunction<Double>();
 
     public static Function<Integer, String> intToString() {
         return INT_TO_STRING_FUNCTION;
@@ -12,6 +13,10 @@ public class NumberFunctions {
 
     public static Function<Long, String> longToString() {
         return LONG_TO_STRING_FUNCTION;
+    }
+
+    public static Function<Double, String> doubleToString() {
+        return DOUBLE_TO_STRING_FUNCTION;
     }
 
     private static class ToStringFunction<T> extends NoArityFunction<T, String> {
--- src/test/java/com/github/dreamhead/jfun/NumberFunctionsTest.java
+++ src/test/java/com/github/dreamhead/jfun/NumberFunctionsTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static com.github.dreamhead.jfun.NumberFunctions.doubleToString;
 import static com.github.dreamhead.jfun.NumberFunctions.intToString;
 import static com.github.dreamhead.jfun.NumberFunctions.longToString;
 import static com.google.common.collect.ImmutableList.of;
@@ -31,5 +32,15 @@ public class NumberFunctionsTest {
         assertThat(Iterables.get(strings, 0), is("1"));
         assertThat(Iterables.get(strings, 1), is("2"));
         assertThat(Iterables.get(strings, 2), is("3"));
+    }
+
+    @Test
+    public void should_convert_double_to_string() {
+        List<Double> doubles = of(1.1, 2.2, 3.3);
+
+        Iterable<String> strings = transform(doubles, doubleToString());
+        assertThat(Iterables.get(strings, 0), is("1.1"));
+        assertThat(Iterables.get(strings, 1), is("2.2"));
+        assertThat(Iterables.get(strings, 2), is("3.3"));
     }
 }
